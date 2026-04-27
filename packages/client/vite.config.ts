26bfb784a27f24d381fae882dabf42b0515a9dcd
nedoxff <nedoxff@proton.me>
2026-04-28 00:46:09 +0300

    buy a domain yay :D

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
-  ],
-})
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+		preview: {
+			port: Number(env.PORT),
+			strictPort: true,
+		},
+		server: {
+			port: Number(env.PORT),
+			strictPort: true,
+			allowedHosts: [env.HOSTNAME],
+		},
+	};
+});
